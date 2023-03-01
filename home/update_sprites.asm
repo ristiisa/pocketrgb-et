8a98e5ffc6e801a17147c8e6b5b29b88dc1e4c31
Dracrius <a.achorn@gmail.com>
2023-03-01 00:26:52 -0500

    Allow the OAM updater to be skipped during V-Blank.

--- home/update_sprites.asm
+++ home/update_sprites.asm
@@ -2,6 +2,8 @@ UpdateSprites::
 	ld a, [wUpdateSpritesEnabled]
 	dec a
 	ret nz
+	ld hl, hSkipOAMUpdates
+	set 0, [hl]
 	ldh a, [hLoadedROMBank]
 	push af
 	ld a, BANK(_UpdateSprites)
@@ -11,4 +13,6 @@ UpdateSprites::
 	pop af
 	ldh [hLoadedROMBank], a
 	ld [MBC1RomBank], a
+	ld hl, hSkipOAMUpdates
+	res 0, [hl]
 	ret
