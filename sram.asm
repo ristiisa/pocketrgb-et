40ae867d6df1c3d216885e87f66db9bf5ba75845
Dracrius <a.achorn@gmail.com>
2023-03-10 04:21:23 -0500

    Reverted Boxes to 12 by 20 as it some how overloaded the ram will investigate later

--- sram.asm
+++ sram.asm
@@ -27,14 +27,19 @@ sBox1:: ds wBoxDataEnd - wBoxDataStart
 sBox2:: ds wBoxDataEnd - wBoxDataStart
 sBox3:: ds wBoxDataEnd - wBoxDataStart
 sBox4:: ds wBoxDataEnd - wBoxDataStart
+sBox5:: ds wBoxDataEnd - wBoxDataStart
+sBox6:: ds wBoxDataEnd - wBoxDataStart
 sBank2AllBoxesChecksum:: ds 1
-sBank2IndividualBoxChecksums:: ds 4
+sBank2IndividualBoxChecksums:: ds 6
 
 
 SECTION "Saved Boxes 2", SRAM ; BANK 3
-sBox5:: ds wBoxDataEnd - wBoxDataStart
-sBox6:: ds wBoxDataEnd - wBoxDataStart
+
 sBox7::  ds wBoxDataEnd - wBoxDataStart
 sBox8::  ds wBoxDataEnd - wBoxDataStart
+sBox9::  ds wBoxDataEnd - wBoxDataStart
+sBox10:: ds wBoxDataEnd - wBoxDataStart
+sBox11:: ds wBoxDataEnd - wBoxDataStart
+sBox12:: ds wBoxDataEnd - wBoxDataStart
 sBank3AllBoxesChecksum:: ds 1
-sBank3IndividualBoxChecksums:: ds 4
+sBank3IndividualBoxChecksums:: ds 6
