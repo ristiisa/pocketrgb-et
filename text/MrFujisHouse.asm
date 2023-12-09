54b977ebab464cc2e55125b0cf31ede92f23a22c
Silver Aabjõe <ristiisa@gmail.com>
2023-12-09 18:18:57 +0200

    vahetõlked

--- text/MrFujisHouse.asm
+++ text/MrFujisHouse.asm
@@ -1,10 +1,12 @@
 _LavenderHouse1Text_1d8d1::
+;;Kummaline, HR.FUJI polegi siin. Kuhu ta küll läks?
 	text "That's odd, ELDER"
 	line "FUJI isn't here."
 	cont "Where'd he go?"
 	done
 
 _LavenderHouse1Text_1d8d6::
+;;HR.FUJI palvetas CUBONEi ema eest.
 	text "ELDER FUJI had"
 	line "been praying alone"
 	cont "for KARAKARA's"
@@ -12,24 +14,28 @@ _LavenderHouse1Text_1d8d6::
 	done
 
 _LavenderHouse1Text_1d8f4::
+;;See ongi HR.FUJI kodu.
 	text "This is really"
 	line "ELDER FUJI's house"
 
+;;Ta on väga lahke!
 	para "He's really kind!"
 
+;;Ta hoolitseb #MONide eest, mis on mahajäetud või vanemad kaotanud!
 	para "He looks after"
 	line "abandoned and"
 	cont "orphaned #MON!"
 	done
 
 _LavenderHouse1Text_1d8f9::
+;;Nii mõnus ja soe! #MONe on nii tore kallistada!
 	text "It's so warm!"
 	line "#MON are so"
 	cont "nice to hug!"
 	done
 
 _LavenderHouse1Text3::
-	text "KODUCK: Gwappa!@"
+	text "PSYDUCK: Gwappa!@"
 	text_end
 
 _LavenderHouse1Text4::
@@ -37,13 +43,15 @@ _LavenderHouse1Text4::
 	text_end
 
 _LavenderHouse1Text_1d94c::
-	text "MR.FUJI: <PLAYER>."
+	text "HR.FUJI: <PLAYER>."
 
+;;Sinu #DEX ei täitu ilma armastuseta #MONide vastu.
 	para "Your #DEX quest"
 	line "may fail without"
 	cont "love for your"
 	cont "#MON."
 
+;;Arvan, et sellest võib sulle su teekonnal abi olla.
 	para "I think this may"
 	line "help your quest."
 	prompt
@@ -58,33 +66,40 @@ _ReceivedFluteText::
 _FluteExplanationText::
 	text_start
 
+;;Kõik magavad #MONid kes kuulevad #VILE häält, ärkavad.
 	para "Upon hearing #"
 	line "FLUTE, sleeping"
 	cont "#MON will"
 	cont "spring awake."
 
+;;See töötab iga magava #MONiga
 	para "It works on all"
 	line "sleeping #MON."
 	done
 
 _FluteNoRoomText::
+;;Tee enda kotis selle jaoks ruumi!
 	text "You must make"
 	line "room for this!"
 	done
 
 _MrFujiAfterFluteText::
+;;HR.FUJI: Kas mu vile aitas sind?
 	text "MR.FUJI: Has my"
 	line "FLUTE helped you?"
 	done
 
 _LavenderHouse1Text6::
+;;#MON iga kuuline joonistamis võistlus!
 	text "#MON Monthly"
 	line "Grand Prize"
 	cont "Drawing!"
 
+;;Osalevis avaldus on ...
 	para "The application"
 	line "form is..."
 
+;;Kadunud! See on välja lõigatud!
 	para "Gone! It's been"
 	line "clipped out!"
 	done
