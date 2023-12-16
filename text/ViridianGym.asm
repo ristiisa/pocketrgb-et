aa52feebf091eeb47a1dc1b887bac56d78dd8542
Silver Aabjõe <ristiisa@gmail.com>
2023-12-16 23:20:38 +0200

    vahetõlked

--- text/ViridianGym.asm
+++ text/ViridianGym.asm
@@ -1,17 +1,21 @@
 _ViridianGymText_74ace::
+;;Hahahaha! See on minu peidupaik!
 	text "Fwahahaha! This is"
 	line "my hideout!"
 
+;;Ma plaanisin just siin RAKETI KAMBA taasasutada!
 	para "I planned to"
 	line "resurrect ROCKET"
 	cont "GANG here!"
 
+;;Aga jälle, sa oled mulle jälile jõudnud! Seekord ma ennast tagasi ei hoia!
 	para "But, you have"
 	line "caught me again!"
 	cont "So be it! This"
 	cont "time, I'm not"
 	cont "holding back!"
 
+;;Veel kord kohtad sa võitluses SAKAKIT, kõige paremat treenerit!
 	para "Once more, you"
 	line "shall face"
 	cont "SAKAKI, the"
@@ -20,6 +24,7 @@ _ViridianGymText_74ace::
 
 _ViridianGymText_74ad3::
 	text "Ha!"
+;;See oli tõesti pingeline võtlus! Sa võitsid! Võidu kinnituseks sulle, HALLMÄRK!
 	line "That was a truly"
 	cont "intense fight!"
 	cont "You have won!"
@@ -28,34 +33,41 @@ _ViridianGymText_74ad3::
 	text_end
 
 _ViridianGymText_74ad9::
+;;Ma ei saa enam kunagi oma käsilastele otsa vaadata, kuna kaotasin! RAKETTI KAMP on igavesti läbi!
 	text "Having lost, I"
 	line "cannot face my"
 	cont "underlings!"
 	cont "ROCKET GANG is"
 	cont "finished forever!"
 
+;;Oma edasise elu pühendan ma #MONide uurimisele!
 	para "I will dedicate my"
 	line "life to the study"
 	cont "of #MON!"
 
+;;Äkki kohtume veel kunagi! Hüvasti!
 	para "Let us meet again"
 	line "some day!"
 	cont "Farewell!@"
 	text_end
 
 _ViridianGymText12::
+;;ROHEMÄRKI omades kuuletuvad kõik #MONid sõltumata tasemest!
 	text "The GREENBADGE"
 	line "makes #MON of"
 	cont "any level obey!"
 
+;;See on tõestuseks su #MON meistriks olekul!
 	para "It is evidence of"
 	line "your mastery as a"
 	cont "#MON trainer!"
 
+;;Selle märgi ettenäitamisel saad sa #MON liigasse!
 	para "With it, you can"
 	line "enter the #MON"
 	cont "LEAGUE!"
 
+;;See on minu kink sulle, sinu #MON LIIGA väljakutsel!
 	para "It is my gift for"
 	line "your #MON"
 	cont "LEAGUE challenge!"
@@ -69,71 +81,82 @@ _ReceivedTM27Text::
 _TM27ExplanationText::
 	text_start
 
+;;TM27 on MAALÕHE! See võte saab ühe hoobiga jagu suvalisest #MONist!
 	para "TM27 is FISSURE!"
 	line "It will take out"
 	cont "#MON with just"
 	cont "one hit!"
 
+;;Ma lõin selle siis, kui siinset SAALI juhtisin, liiga kaua aega tagasi...
 	para "I made it when I"
 	line "ran the GYM here,"
 	cont "too long ago..."
 	done
 
 _TM27NoRoomText::
+;;Sul pole selle jaoks ruumi!
 	text "You do not have"
 	line "space for this!"
 	done
 
 _ViridianGymBattleText1::
+;;Heh! Nüüdseks peaks sul juba toss väljas olema!
 	text "Heh! You must be"
 	line "running out of"
 	cont "steam by now!"
 	done
 
 _ViridianGymEndBattleText1::
-	text "I"
-	line "ran out of gas!"
+	text "Mul"
+	line "sai jaks otsa!"
 	prompt
 
 _ViridianGymAfterBattleText1::
+;;Selleks, et meie SAALI JUHIGA sammu pidada, peab sul jaksu olema!
 	text "You need power to"
 	line "keep up with our"
 	cont "GYM LEADER!"
 	done
 
 _ViridianGymBattleText2::
+;;Uhh Uhh Urr! Ma kütan, et raevu!
 	text "Rrrroar! I'm"
 	line "working myself"
 	cont "into a rage!"
 	done
 
 _ViridianGymEndBattleText2::
-	text "Wargh!"
+	text "Argh!"
 	prompt
 
 _ViridianGymAfterBattleText2::
+;;Ma pole ikka veel piisav!
 	text "I'm still not"
 	line "worthy!"
 	done
 
 _ViridianGymBattleText3::
+;;#MONid ja mina, me teeme koos imelist muusikat!
 	text "#MON and I, we"
 	line "make wonderful"
 	cont "music together!"
 	done
 
 _ViridianGymEndBattleText3::
+;;Sa oled täiuslikus harmoonias!
 	text "You are in"
 	line "perfect harmony!"
 	prompt
 
 _ViridianGymAfterBattleText3::
+;;Kas tead kes on meie SAALI JUHT?
 	text "Do you know the"
 	line "identity of our"
 	cont "GYM LEADER?"
 	done
 
 _ViridianGymBattleText4::
+;;Karate on ülim vorm võitluskunstidest!
 	text "Karate is the"
 	line "ultimate form of"
 	cont "martial arts!"
@@ -144,59 +167,67 @@ _ViridianGymEndBattleText4::
 	prompt
 
 _ViridianGymAfterBattleText4::
+;;Kui mu #MONid oleks nii head karates kui mina...
 	text "If my #MON"
 	line "were as good at"
 	cont "Karate as I..."
 	done
 
 _ViridianGymBattleText5::
+;;Talendikad võidavad stiilselt!
 	text "The truly talented"
 	line "win with style!"
 	done
 
 _ViridianGymEndBattleText5::
-	text "I"
-	line "lost my grip!"
+	text "Mu"
+	line "haare libises!"
 	prompt
 
 _ViridianGymAfterBattleText5::
+;;Me JUHT kurjustab nüüd minuga!
 	text "The LEADER will"
 	line "scold me!"
 	done
 
 _ViridianGymBattleText6::
+;;Mina olen KARATE KUNN! Su saatus on minu kätes!
 	text "I'm the KARATE"
 	line "KING! Your fate"
 	cont "rests with me!"
 	done
 
 _ViridianGymEndBattleText6::
-	text "Ayah!"
+	text "Aiahh!"
 	prompt
 
 _ViridianGymAfterBattleText6::
+;;Sina ja #MONi LIIGA? Ära tee nalja!
 	text "#MON LEAGUE?"
 	line "You? Don't get"
 	cont "cocky!"
 	done
 
 _ViridianGymBattleText7::
+;;Su #MONid värisevad mu piitsa palksust!
 	text "Your #MON will"
 	line "cower at the"
 	cont "crack of my whip!"
 	done
 
 _ViridianGymEndBattleText7::
-	text "Yowch!"
-	line "Whiplash!"
+	text "Vušš!"
+	line "Sahmaki!"
 	prompt
 
 _ViridianGymAfterBattleText7::
+;;Ootand nüüd! Ma olin lihtsalt hooletu!
 	text "Wait! I was just"
 	line "careless!"
 	done
 
 _ViridianGymBattleText8::
+;;TOKIWA SAAL oli kaua suletud, kui nüüd me JUHT on naasnud!
 	text "TOKIWA GYM was"
 	line "closed for a long"
 	cont "time, but now our"
@@ -204,11 +235,12 @@ _ViridianGymBattleText8::
 	done
 
 _ViridianGymEndBattleText8::
-	text "I"
-	line "was beaten?"
+	text "Ma"
+	line "sain lüüa?"
 	prompt
 
 _ViridianGymAfterBattleText8::
+;;Sa saad minna #MON LIIGASSE ainult peale meie SAALI JUHI võitmist!
 	text "You can go onto"
 	line "#MON LEAGUE"
 	cont "only by defeating"
@@ -216,17 +248,21 @@ _ViridianGymAfterBattleText8::
 	done
 
 _ViridianGymText_74bd4::
+;;Yo! Tulevane meister!
 	text "Yo! Champ in"
 	line "making!"
 
+;;Isegi mina ei tea kes on TOKIWA JUHI tegelikult!
 	para "Even I don't know"
 	line "TOKIWA LEADER's"
 	cont "identity!"
 
+;;See saab olema kõigist SAALI JUHI võitlustest karmim!
 	para "This will be the"
 	line "toughest of all"
 	cont "the GYM LEADERs!"
 
+;;Ma kuulsin, et siinsed treenerid eelistavad maa tüüpi #MONe!
 	para "I heard that the"
 	line "trainers here"
 	cont "like ground-type"
@@ -234,6 +270,7 @@ _ViridianGymText_74bd4::
 	done
 
 _ViridianGymText_74bd9::
+;;Ära aja! SAKAKI oli meie SAALI JUHT?
 	text "Blow me away!"
 	line "SAKAKI was the"
 	cont "GYM LEADER here?"
