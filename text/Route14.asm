6aa731e94bd7cb93be70499213cd7de6f4ad5f72
Silver Aabjõe <ristiisa@gmail.com>
2023-12-21 13:11:43 +0200

    vahetõlked

--- text/Route14.asm
+++ text/Route14.asm
@@ -1,15 +1,17 @@
 _Route14BattleText1::
+;;Selleks, et #MONid oskaks häid võtteid pead kasutama TMe!
 	text "You need to use"
 	line "TMs to teach good"
 	cont "moves to #MON!"
 	done
 
 _Route14EndBattleText1::
-	text "Not"
-	line "good enough!"
+	text "Pole"
+	line "piisavalt hea!"
 	prompt
 
 _Route14AfterBattleText1::
+;;Sul on ju mõni HM? #MONid ei unusta neid võtteid kunagi.
 	text "You have some HMs"
 	line "right? #MON"
 	cont "can't ever forget"
@@ -17,22 +19,25 @@ _Route14AfterBattleText1::
 	done
 
 _Route14BattleText2::
+;;Mu lind #MONid peaks võitluseks valmis olema.
 	text "My bird #MON"
 	line "should be ready"
 	cont "for battle."
 	done
 
 _Route14EndBattleText2::
-	text "Not"
-	line "ready yet!"
+	text "Mitte"
+	line "piisavalt valmis!"
 	prompt
 
 _Route14AfterBattleText2::
+;;Nad peavad paremaid võtteid õppima.
 	text "They need to learn"
 	line "better moves."
 	done
 
 _Route14BattleText3::
+;;TMe saad osta TAMAMUSHI linnas! Aga vähestel on HMe!
 	text "TMs are on sale"
 	line "in TAMAMUSHI!"
 	cont "But, only a few"
@@ -40,11 +45,12 @@ _Route14BattleText3::
 	done
 
 _Route14EndBattleText3::
-	text "Aww,"
-	line "bummer!"
+	text "Njaa"
+	line "jamps!"
 	prompt
 
 _Route14AfterBattleText3::
+;;Kui sa õpetad #MONile sama elemendi tüüpi võtteid, siis need on võimsamad.
 	text "Teach #MON"
 	line "moves of the same"
 	cont "element type for"
@@ -52,122 +58,138 @@ _Route14AfterBattleText3::
 	done
 
 _Route14BattleText4::
+;;Kas sa oled oma lind #MONile õpetanud LENDAMIST?
 	text "Have you taught"
 	line "your bird #MON"
 	cont "how to FLY?"
 	done
 
 _Route14EndBattleText4::
-	text "Shot"
-	line "down in flames!"
+	text "Maas"
+	line "leekides!"
 	prompt
 
 _Route14AfterBattleText4::
+;;Mu eluarmastus on lind #MONid!
 	text "Bird #MON are"
 	line "my true love!"
 	done
 
 _Route14BattleText5::
+;;Oled sa kuulnud legendaarsetest #MONidest?
 	text "Have you heard of"
 	line "the legendary"
 	cont "#MON?"
 	done
 
 _Route14EndBattleText5::
-	text "Why?"
-	line "Why'd I lose?"
+	text "Miks?"
+	line "Miks ma kaotasin?"
 	prompt
 
 _Route14AfterBattleText5::
+;;Kõik legendaarsed #MONid on linnud.
 	text "The 3 legendary"
 	line "#MON are all"
 	cont "birds."
 	done
 
 _Route14BattleText6::
+;;Pole just minu teema aga OK! Läki!
 	text "I'm not into it,"
 	line "but OK! Let's go!"
 	done
 
 _Route14EndBattleText6::
-	text "I"
-	line "knew it!"
+	text "Tea-"
+	line "dsin seda!"
 	prompt
 
 _Route14AfterBattleText6::
+;;Võit või kaotus... pikas perspektiivis pole seal tähtsust!
 	text "Winning, losing,"
 	line "it doesn't matter"
 	cont "in the long run!"
 	done
 
 _Route14BattleText7::
+;;Tule, tule, läks, läks, läks!
 	text "C'mon, c'mon."
 	line "Let's go, let's"
 	cont "go, let's go!"
 	done
 
 _Route14EndBattleText7::
-	text "Arrg!"
-	line "Lost! Get lost!"
+	text "Uff!"
+	line "Kaotus! Kao ära!"
 	prompt
 
 _Route14AfterBattleText7::
+;;Mis, mis mis? Mida sa tahad?
 	text "What, what, what?"
 	line "What do you want?"
 	done
 
 _Route14BattleText8::
+;;Suurepärane! Mul ongi vaja aega surnuks lüüa!
 	text "Perfect! I need to"
 	line "burn some time!"
 	done
 
 _Route14EndBattleText8::
-	text "What?"
-	line "You!?"
+	text "Mis?"
+	line "Sina!?"
 	prompt
 
 _Route14AfterBattleText8::
+;;#MONide kasvatamine on ilge tüütus!
 	text "Raising #MON"
 	line "is a drag, man."
 	done
 
 _Route14BattleText9::
+;;Me oleme siin, sest siin on rohkem ruumi!
 	text "We ride out here"
 	line "because there's"
 	cont "more room!"
 	done
 
 _Route14EndBattleText9::
-	text "Wipe out!"
+	text "Plats"
+	line "puhas!"
 	prompt
 
 _Route14AfterBattleText9::
+;;Nii lahe, et sa oma #MONid nii tugevaks oled treeninud!
 	text "It's cool you"
 	line "made your #MON"
 	cont "so strong!"
 
+;;Väel on võimu! Ja sinagi tead seda!
 	para "Might is right!"
 	line "And you know it!"
 	done
 
 _Route14BattleText10::
+;;#MON võitlus! Lahe! Müdistama!
 	text "#MON fight?"
 	line "Cool! Rumble!"
 	done
 
 _Route14EndBattleText10::
-	text "Blown"
-	line "away!"
+	text "Pü-"
+	line "hitud minema!"
 	prompt
 
 _Route14AfterBattleText10::
+;;Sina ja mina... ma tean kes võidaks!
 	text "You know who'd"
 	line "win, you and me"
 	cont "one on one!"
 	done
 
 _Route14Text11::
-	text "ROUTE 14"
-	line "West to SEKICHIKU"
-	cont "CITY"
+	text "TEE 14"
+	line "SEKICHIKU"
+	cont "LINN"
 	done
