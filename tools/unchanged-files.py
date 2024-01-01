f5f898eb0e72664755a49e2b14cdc4e9c377490c
Silver Aabjõe <ristiisa@gmail.com>
2024-01-01 15:41:30 +0200

    täis tee failidele

--- tools/unchanged-files.py
+++ tools/unchanged-files.py
@@ -16,7 +16,7 @@ for path, _, files in walk(sys.argv[1]):
 
         r = subprocess.run(["git", "diff", "--quiet", "5ca726479830da61dc03b86513f7b4411756d848..HEAD", "--", join(path, name)])
         if r.returncode == 1: continue
-        print([name, r.returncode])
+        print([join(path, name), r.returncode])
         with open(join(path, name), encoding='utf-8', errors='ignore') as file:
             lines = [line for i, line in enumerate(file) if line.strip().lower().startswith((
                 "db",
