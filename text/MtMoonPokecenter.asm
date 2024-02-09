6e7a21c0d7624cc35ed259b583745d7462ce1fd1
ristiisa <ristiisa@gmail.com>
2024-02-09 13:24:43 +0200

    veel tõlkeid

--- text/MtMoonPokecenter.asm
+++ text/MtMoonPokecenter.asm
@@ -1,27 +1,33 @@
 _MtMoonPokecenterText1::
+;;Mul on 6 KOLLI PALLI mu vööl.
 	text "I've 6 MONSTER"
 	line "BALLs set in"
 	cont "my belt."
 
+;;Rohkem kui 6 #MONi vööle ei mahu.
 	para "At most, you can"
 	line "carry 6 #MON."
 	done
 
 _MtMoonPokecenterText3::
+;;RAKETI KAMP ründab hanada kodanike...
 	text "ROCKET GANG"
 	line "attacks HANADA"
 	cont "citizens..."
 
+;;RAKETI KAMP on alati uudistes!
 	para "ROCKET GANG is"
 	line "always in the"
 	cont "news!"
 	done
 
 _MagikarpSalesmanText1::
+;;MEES: Terekest! Kas mul pole just sulle üks hea pakkumine!
 	text "MAN: Hello, there!"
 	line "Have I got a deal"
 	cont "just for you!"
 
+;;Ma annan sulle ühe toreda MAGIKARPi ¥500 eest! Mida ütled?
 	para "I'll let you have"
 	line "a swell KOIKING"
 	cont "for just ¥500!"
@@ -29,17 +35,20 @@ _MagikarpSalesmanText1::
 	done
 
 _MagikarpSalesmanNoText::
+;;Ei? Ma teen seda ainult teene pärast sinule!
 	text "No? I'm only"
 	line "doing this as a"
 	cont "favor to you!"
 	done
 
 _MagikarpSalesmanNoMoneyText::
+;;Kuule! Natuke rohkem ikka raha on vaja!
 	text "You'll need more"
 	line "money than that!"
 	done
 
 _MagikarpSalesmanText2::
+;;MEES: Nojah, kauba tagastust ei toimu!
 	text "MAN: Well, I don't"
 	line "give refunds!"
 	done
--- text/Museum1F.asm
+++ text/Museum1F.asm
@@ -1,35 +1,43 @@
 _Museum1FText_5c21a::
+;;Külastage meid jälle!
 	text "Come again!"
 	done
 
 _Museum1FText_5c21f::
+;;Lastepilet on ¥50.
 	text "It's ¥50 for a"
 	line "child's ticket."
 
+;;Soovid sa siseneda?
 	para "Would you like to"
 	line "come in?"
 	done
 
 _Museum1FText_5c224::
+;;Nonii, ¥50! Aitäh!
 	text "Right, ¥50!"
 	line "Thank you!"
 	done
 
 _Museum1FText_5c229::
+;;Aga sul pole piisavalt raha.
 	text "You don't have"
 	line "enough money."
 	prompt
 
 _Museum1FText_5c22e::
+;;Varuväljapääsu kaudu sisse küll ei hiilita!
 	text "You can't sneak"
 	line "in the back way!"
 
+;;Ah, tead, mida iganes! Kas sa tead, mis MEREVAIK on?
 	para "Oh, whatever!"
 	line "Do you know what"
 	cont "AMBER is?"
 	done
 
 _Museum1FText_5c233::
+;;Kuskil olla üks labor, mis üritab iidseid #MONe merevaigust üles äratada.
 	text "There's a lab"
 	line "somewhere trying"
 	cont "to resurrect"
@@ -38,23 +46,28 @@ _Museum1FText_5c233::
 	done
 
 _Museum1FText_5c238::
+;;Merevaik on kivistunud puu vaik.
 	text "AMBER is fossil-"
 	line "ized tree sap."
 	done
 
 _Museum1FText_5c23d::
+;;Palun mine teisele poole!
 	text "Please go to the"
 	line "other side!"
 	done
 
 _Museum1FText_5c242::
+;;Piisavalt aega, et siin ringi vaadata!
 	text "Take plenty of"
 	line "time to look!"
 	done
 
 _Museum1FText_5c251::
+;;Taevas tänatud!
 	text "Thank heavens!"
 	
+;;Ma pole iialgi arvanud, et ma näen Draakonjumala luid!
     para "I never thought I"
 	line "would ever look"
     cont "upon the Dragon"
@@ -62,43 +75,51 @@ _Museum1FText_5c251::
 	done
 
 _Museum1FText_5c28e::
+;;Tss! Ma arvan, et see tükike MEREVAIKU sisaldab #MON DNAd!
 	text "Ssh! I think that"
 	line "this chunk of"
 	cont "AMBER contains"
 	cont "#MON DNA!"
 
+;;Nii vahva oleks, kui #MONe saaks sellest taaselustada!
 	para "It would be great"
 	line "if #MON could"
 	cont "be resurrected"
 	cont "from it!"
 
+;;Aga mu kolleegid lihtsalt ignoreerivad mind!
 	para "But, my colleagues"
 	line "just ignore me!"
 
+;;Niisiis, mul on üks teene paluda!
 	para "So I have a favor"
 	line "to ask!"
 
+;;Vii see #MON LABORISSE analüüsida!
 	para "Take this to a"
 	line "#MON LAB and"
 	cont "get it examined!"
 	prompt
 
 _ReceivedOldAmberText::
-	text "<PLAYER> received"
-	line "OLD AMBER!@"
+	text "<PLAYER> sai"
+	line "MEREVAIK!@"
 	text_end
 
 _Museum1FText_5c299::
+;;Tss! Mine ja lase seda MEREVAIKU analüüsida!
 	text "Ssh! Get the OLD"
 	line "AMBER checked!"
 	done
 
 _Museum1FText_5c29e::
+;;Aga sul pole ju selleks ruumi!
 	text "You don't have"
 	line "space for this!"
 	done
 
 _Museum1FText_5c2ad::
+;;Me oleme kahe väga vana #MONi kivistise uhked omanikud!
 	text "We are proud of 2"
 	line "fossils of very"
 	cont "rare, prehistoric"
@@ -106,6 +127,7 @@ _Museum1FText_5c2ad::
 	done
 
 _Museum1FText_5c2bc::
+;;Merevaik on kuldne ja läbipaistev!
 	text "The AMBER is"
 	line "clear and gold!"
 	done
